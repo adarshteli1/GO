025a1e7e9041a9700073f14620631a28d0ba2c6a
Addy <addy@kali.kali>
2026-05-21 02:26:18 -0400

    Reading one file at a Time

--- projects/concurrentreader/main.go
+++ projects/concurrentreader/main.go
@@ -4,36 +4,50 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	files := []string{
-		"file1.txt",
-		"file2.txt",
-		"file3.txt",
-	}
 
-	for _, file := range files {
-		file, err := os.Open(file)
-		if err != nil {
-			fmt.Println("Error ", err)
-		} else {
-			fmt.Println("All Good")
-		}
+	Read("file1.txt")
+	// files := []string{
+	// 	"file1.txt",
+	// 	"file2.txt",
+	// 	"file3.txt",
+	// }
 
-		Read(file)
-	}
+	// for _, file := range files {
+	// 	file, err := os.Open(file)
+	// 	if err != nil {
+	// 		fmt.Println("Error ", err)
+	// 	} else {
+	// 		fmt.Println("All Good")
+	// 	}
+
+	// 	Read(file)
+	// }
 }
 
-func Read(file *os.File) {
-	Words := len(file)
+func Read(file string) {
+	charCount := 0
+	wordCount := 0
 	count := 0
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		count++
+	openedFile, err := os.Open(file)
+	if err != nil {
+		fmt.Println("Error", err)
+	} else {
+		scanner := bufio.NewScanner(openedFile)
+		for scanner.Scan() {
+			line := scanner.Text()
+			count++
+			wordCount += len(strings.Fields(line))
+			charCount += len([]rune(line))
+		}
+		fmt.Println("Lines are :", count)
+		fmt.Println("Characters are :", charCount)
+		fmt.Println("Words are :", wordCount)
 	}
-	fmt.Println("Lines are :", count)
-	fmt.Println("Words are :", Words)
+
 }
 
 type Reader struct {
